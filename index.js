bd044dca689192045e1428981a738c57e9230ca4
Sameer Kanakia <skanakia1@gmail.com>
2018-03-19 17:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,4 @@ var height = parseInt("70")
 // Don't worry about this
 module.exports = { name, height
 }
-if (name === "Susan") {
-  console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
+
